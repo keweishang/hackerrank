3203c782ee9df296d9137fbb25b7837f2c153c08
Kewei Shang <kewei.shang@gmail.com>
2016-06-17 20:12:27 +0200

    add new syntax

--- python_syntax.py
+++ python_syntax.py
@@ -4,3 +4,6 @@ name,space,price = raw_input().rpartition(' ')
 # Iterate through dict items, each item is a tuple
 for item_name, net_price in my_order.items():
     print item_name, net_price
+
+# Create a list of list using for loop
+b = [[1,2,3,4,5,6,7] for _ in range(n)]
